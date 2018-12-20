0c15dec3e388b0c29d61a1c715b91d29aaea3a7c
MatskoAlexander <AMatsko54@mail.ru>
2018-12-20 19:44:31 +0700

    works even better

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -30,7 +30,7 @@ try:
         with open('Text.txt', 'r') as text:
             text = text.read()
         answer = input('\nWhat do you want to do? / Что вы хотите сделать?\n1)Encode / Закодировать\n2)Decode / '
-                   'Раскодировать\n3)Add language / Добавить язык\n4)Show alphabet list / Показать список алфавитов\n'
+                   'Раскодировать\n3)Show alphabet list / Показать список алфавитов\n4)Add language / Добавить язык\n'
                        '5)End program / Завершить программу\n')
         if answer == '1' or answer.lower() == 'encode' or answer.lower() == 'закодировать':
             k = 'k'
@@ -44,7 +44,7 @@ try:
                 k = input('Enter shift (integer) / Введите сдвиг (целое число): ')
             k = int(k)
             decode(k, text)
-        elif answer == '3' or answer.lower() == 'add language' or answer.lower() == 'добавить язык':
+        elif answer == '4' or answer.lower() == 'add language' or answer.lower() == 'добавить язык':
             answer2 = input('In the initial version of the program there are 2 languages: English and Russian. '
                             'Want to add something? /\nВ изначальной версии программы есть 2 языка: английский и '
                             'русский. Хотите что-то добавить?\n1)Yes / Да\n2)No / Нет\n')
@@ -56,7 +56,7 @@ try:
                 else:
                     new_list = [new_alphabet]
                 languages.extend(new_list)
-        elif answer == '4' or answer.lower() == 'show alphabet list' or answer.lower() == 'показать список алфавитов':
+        elif answer == '3' or answer.lower() == 'show alphabet list' or answer.lower() == 'показать список алфавитов':
             print(languages)
         elif answer == '5' or answer.lower() == 'end program' or answer.lower() == 'завершить программу':
             break
